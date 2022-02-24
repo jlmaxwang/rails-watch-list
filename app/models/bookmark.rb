9317d8a57494f3fcf9c642eaea0f549e3bdcf9cb
Max Jilu Wang <wangjilu23@gmail.com>
2022-02-24 16:07:31 +1100

    movie, list, bookmark models created

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,19 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  # before_destroy :prevent_destroy
-  # attr_accessor :destroyed_by_parent
 
   validates :comment, presence: true, length: { minimum: 6 }
   validates :movie, :list, presence: true
   validates :movie_id, uniqueness: { scope: :list_id }
-
-  # private
-
-  # def prevent_destroy
-  #   if !destroyed_by_parent
-  #     self.errors[:base] << "You may not delete this child."
-  #     return false
-  #   end
-  # end
 end
