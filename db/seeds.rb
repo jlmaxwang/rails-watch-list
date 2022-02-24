069020f98b0551e9f64eda411720b935cf9f8bc1
Max Jilu Wang <wangjilu23@gmail.com>
2022-02-24 16:08:08 +1100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Movie.create({ title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: Faker::Internet.url, rating: Faker::Number.decimal(l_digits: 1) })
+end
